f00771a6a3676ff2af69e6d88fa7fa05831a926c
Andrew Evdokimenko <evdokimenko_9797@mail.ru>
2024-03-11 20:53:48 +0300

    Initial commit

---
+++ api-tests/src/test/java/ApiTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ApiTests {
+    @Test
+    public void testAPI() {
+        System.out.println("API тест запущен!");
+    }
+
+    @Test
+    @Tag("smoke")
+    public void testAPISmoke() {
+        System.out.println("Smoke API тест запущен!");
+    }
+}
